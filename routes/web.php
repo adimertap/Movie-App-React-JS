4d95b1d2bdae0864cfaaba64e9cc8a99274c9758
Adim <50594388+adimertap@users.noreply.github.com>
2022-09-23 21:13:08 +0800

    feat: slicing subscription plan

--- routes/web.php
+++ routes/web.php
@@ -33,6 +33,10 @@ Route::prefix('prototype')->name('prototype.')->group(function(){
     Route::get('/dashboard', function(){
         return Inertia::render('Prototype/Dashboard');
     })->name('dashboard');
+
+    Route::get('/subscriptionPlan', function(){
+        return Inertia::render('Prototype/SubscriptionPlan');
+    })->name('subscriptionPlan');
     
 });
 
