7563d3f9da83fe9dd3e55e7f55333c9acf2f30a9
Adim <50594388+adimertap@users.noreply.github.com>
2022-09-24 00:19:24 +0800

    feat: integration sign-up

--- resources/js/Pages/Auth/Register.jsx
+++ resources/js/Pages/Auth/Register.jsx
@@ -1,12 +1,11 @@
 import React, { useEffect } from 'react';
-import GuestLayout from '@/Layouts/GuestLayout';
-import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
+import Label from '@/Components/InputLabel';
+import Input from '@/Components/TextInput';
+import Button from '@/Components/PrimaryButton';
+import ValidationErrors from '@/Components/ValidationErrors';
 import { Head, Link, useForm } from '@inertiajs/inertia-react';
 
-export default function Register() {
+export default function Register(){
     const { data, setData, post, processing, errors, reset } = useForm({
         name: '',
         email: '',
@@ -21,94 +20,81 @@ export default function Register() {
     }, []);
 
     const onHandleChange = (event) => {
-        setData(event.target.name, event.target.type === 'checkbox' ? event.target.checked : event.target.value);
+        setData(event.target.name, event.target.value);
     };
 
     const submit = (e) => {
         e.preventDefault();
 
+        // console.log(data);
         post(route('register'));
     };
 
-    return (
-        <GuestLayout>
-            <Head title="Register" />
-
-            <form onSubmit={submit}>
-                <div>
-                    <InputLabel forInput="name" value="Name" />
-
-                    <TextInput
-                        type="text"
-                        name="name"
-                        value={data.name}
-                        className="mt-1 block w-full"
-                        autoComplete="name"
-                        isFocused={true}
-                        handleChange={onHandleChange}
-                        required
-                    />
-
-                    <InputError message={errors.name} className="mt-2" />
+return (
+<>
+    <Head title="Sign-Up" />
+    <div className="mx-auto max-w-screen min-h-screen bg-black text-white md:px-10 px-3">
+        <div className="fixed top-[-50px] hidden lg:block">
+            <img src="/images/signup-image.png"
+                className="hidden laptopLg:block laptopLg:max-w-[450px] laptopXl:max-w-[640px]" alt="" />
+        </div>
+        <div className="py-10 flex laptopLg:ml-[680px] laptopXl:ml-[870px]">
+            <div>
+                <img src="/images/moonton-white.svg" alt="" />
+                <div className="my-[40px]">
+                    <div className="font-semibold text-[26px] mb-3">
+                        Sign Up
+                    </div>
+                    <p className="text-base text-[#767676] leading-7">
+                        Explore our new movies and get <br />
+                        the better insight for your life
+                    </p>
+                    <ValidationErrors errors={errors}>
+                    </ValidationErrors>
                 </div>
-
-                <div className="mt-4">
-                    <InputLabel forInput="email" value="Email" />
-
-                    <TextInput
-                        type="email"
-                        name="email"
-                        value={data.email}
-                        className="mt-1 block w-full"
-                        autoComplete="username"
-                        handleChange={onHandleChange}
-                        required
-                    />
-
-                    <InputError message={errors.email} className="mt-2" />
-                </div>
-
-                <div className="mt-4">
-                    <InputLabel forInput="password" value="Password" />
-
-                    <TextInput
-                        type="password"
-                        name="password"
-                        value={data.password}
-                        className="mt-1 block w-full"
-                        autoComplete="new-password"
-                        handleChange={onHandleChange}
-                        required
-                    />
-
-                    <InputError message={errors.password} className="mt-2" />
-                </div>
-
-                <div className="mt-4">
-                    <InputLabel forInput="password_confirmation" value="Confirm Password" />
-
-                    <TextInput
-                        type="password"
-                        name="password_confirmation"
-                        value={data.password_confirmation}
-                        className="mt-1 block w-full"
-                        handleChange={onHandleChange}
-                        required
-                    />
-
-                    <InputError message={errors.password_confirmation} className="mt-2" />
-                </div>
-
-                <div className="flex items-center justify-end mt-4">
-                    <Link href={route('login')} className="underline text-sm text-gray-600 hover:text-gray-900">
-                        Already registered?
-                    </Link>
-
-                    <PrimaryButton className="ml-4" processing={processing}>
-                        Register
-                    </PrimaryButton>
-                </div>
-            </form>
-        </GuestLayout>
-    );
-}
+                <form className="w-[370px]" onSubmit={submit}>
+                    <div className="flex flex-col gap-6">
+                        <div>
+                            
+                            <Label forInput="fullname" value="Full Name"></Label>
+                            <Input type='text' value={data.name} isFocused={true}
+                                name='name' placeholder='Your Full Name' 
+                                handleChange={onHandleChange} required>
+                            </Input>
+                        </div>
+                        <div>
+                            <Label forInput="email" value="Email"></Label>
+                            <Input type='email' name='email' placeholder='Your Email Address' value={data.email}
+                                handleChange={onHandleChange} required></Input>
+                        </div>
+                        <div>
+                            <Label forInput="password" value="Password"></Label>
+                            <Input type="password" name="password" placeholder="Your Password" value={data.password}
+                                handleChange={onHandleChange} required></Input>
+                        </div>
+                        <div>
+                            <Label forInput="password" value="Confirm Password"></Label>
+                            <Input type="password" name="password_confirmation" placeholder="Password Confirmation" value={data.password_confirmation}
+                                handleChange={onHandleChange} required></Input>
+                        </div>
+                    </div>
+                    <div className="grid space-y-[14px] mt-[30px]">
+                        <Button type='submit' processing={processing}>
+                            <span className="text-base font-semibold">
+                                Sign Up
+                            </span>
+                        </Button>
+                        <Link href={route('login')}>
+                            <Button variant='light-outline'>
+                                <span className="text-base text-white">
+                                    Sign In to My Account
+                                </span>
+                            </Button>
+                        </Link>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</>
+)};
